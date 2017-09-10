9ace33b104c89429313543dc906aa6a99f2c3bf7
Diego <darkar315@darkar315-virtual-machine>
2017-09-09 21:45:16 -0500

    Falta radio

--- Gravimetria/Taller2/Correcciones.py
+++ Gravimetria/Taller2/Correcciones.py
@@ -15,6 +15,15 @@ pc3 = 2500
 pc4 = 2700
 pc5 = 2800
 
+# VARIABLES
+
+x1 = hoja1[:, 0]
+x2 = hoja2[:, 0]
+h1 = hoja1[:, 1]
+h2 = hoja2[:, 1]
+g1 = hoja1[:, 2]
+g2 = hoja2[:, 2]
+
 # FUNCION CORRECCION BOURGER
 
 def bourguer(p, h):
@@ -24,15 +33,15 @@ def bourguer(p, h):
 
 # Datos primera hoja
 
-cor_b1 = bourguer(pc1, hoja1[:, 1])
-cor_b2 = bourguer(pc2, hoja1[:, 1])
-cor_b3 = bourguer(pc3, hoja1[:, 1])
-cor_b4 = bourguer(pc4, hoja1[:, 1])
-cor_b5 = bourguer(pc5, hoja1[:, 1])
+cor_b1 = bourguer(pc1, h1)
+cor_b2 = bourguer(pc2, h1)
+cor_b3 = bourguer(pc3, h1)
+cor_b4 = bourguer(pc4, h1)
+cor_b5 = bourguer(pc5, h1)
 
 # Datos segunda hoja
 
-cor_b = bourguer(pc1, hoja2[:, 1])
+cor_b = bourguer(pc1, h2)
 
 # FUNCION CORRECCION AIRE LIBRE
 
@@ -42,52 +51,78 @@ def aire(h):
 
 # Dato primera hoja
 
-cor_a1 = aire(hoja1[:, 1])
+cor_a1 = aire(h1)
 
 # Datos segunda hoja
 
-cor_a2 = aire(hoja2[:, 1])
+cor_a2 = aire(h2)
 
 # ANOMALIA BOURGER
-
 # Datos primera hoja
 
-an_b1 = hoja1[:, 2] + cor_a1 + cor_b1
-an_b2 = hoja1[:, 2] + cor_a1 + cor_b2
-an_b3 = hoja1[:, 2] + cor_a1 + cor_b3
-an_b4 = hoja1[:, 2] + cor_a1 + cor_b4
-an_b5 = hoja1[:, 2] + cor_a1 + cor_b5
+an_b1 = g1 + cor_a1 + cor_b1
+an_b2 = g1 + cor_a1 + cor_b2
+an_b3 = g1 + cor_a1 + cor_b3
+an_b4 = g1 + cor_a1 + cor_b4
+an_b5 = g1 + cor_a1 + cor_b5
 
 # Datos segunda hoja
 
-an_b = hoja2[:, 2] + cor_a2 + cor_b
+an_b = g2 + cor_a2 + cor_b
 
 # GRAFICA DE TOPOGRAFIA
 
-plt.plot(hoja1[:, 0], hoja1[:, 1])
-plt.title("Topografia primera parte del volcan")
-plt.xlabel("$Distancia (m)$")
-plt.ylabel("$Elevacion (m)$")
+plt.plot(x1, h1)
+
+plt.title("$TOPOGRAFIA$ $PRIMERA$ $PARTE$ $DEL$ $VOLCAN$")
+plt.xlabel("$Distancia(m)$")
+plt.ylabel("$Elevacion(m)$")
+#plt.show()
 plt.close()
 
 # GRAFICA METODO NETTLETON
 
-plt.plot(hoja1[:, 0], an_b1, label = "$p_1 = 2655$")
-plt.plot(hoja1[:, 0], an_b2, label = "$p_2 = 2600$")
-plt.plot(hoja1[:, 0], an_b3, label = "$p_3 = 2500$")
-plt.plot(hoja1[:, 0], an_b4, label = "$p_4 = 2700$")
-plt.plot(hoja1[:, 0], an_b5, label = "$p_5 = 2800$")
-plt.legend()
-plt.title("Metodo de Nettleton")
+plt.plot(x1, an_b1, label = "$p_1 = 2655$ $Optima$")
+plt.plot(x1, an_b2, label = "$p_2 = 2600$")
+plt.plot(x1, an_b3, label = "$p_3 = 2500$")
+plt.plot(x1, an_b4, label = "$p_4 = 2700$")
+plt.plot(x1, an_b5, label = "$p_5 = 2800$")
+
+plt.legend(loc = 0, title = "$Densidades$ $Propuestas$")
+plt.title("$METODO$ $DE$ $NETTLETON$")
 plt.xlabel("$Distancia (m)$")
-plt.ylabel("$Anomalia Bourger (mGal)$")
+plt.ylabel("$Anomalia$ $Bourger (mGal)$")
+#plt.show()
 plt.close()
+
+# Half-Width
+
+g_m = np.amax(an_b) / 2
+dif = an_b - g_m
+half = np.argmin(abs(dif))
+
+half_w = x2[half]
+g_med = an_b[half]
 
 # GRAFICA TOPOGRAFIA Y NETTLETON
 
-plt.plot(hoja2[:, 0], an_b, label = "$p = 2655$")
-plt.title("Nettleton")
-plt.xlabel("$Distancia (m)$")
-plt.ylabel("$Anomalia de Bourger (mGal)$")
-plt.legend()
-plt.show()
+plt.plot(x2, an_b, label = "$p = 2655$")
+plt.scatter(half_w, g_med)
+
+plt.title("$ANOMALIA$ $DE$ $BOURGUER$ $EN$ $VOLCAN$")
+plt.xlabel("$Distancia(m)$")
+plt.ylabel("$Anomalia$ $Bourger (mGal)$")
+plt.legend(loc = 0, title = "$Densidad$ $Optima$")
+#plt.show()
+plt.close()
+
+# RADIO DE LA ESFERA
+
+z = 1.306 * half_w
+p_d = 200.
+
+arriba = 3. * an_b * (x2**2 + z**2)**(3/2)
+abajo = 4. * np.pi * G * p_d * z
+
+r = (arriba / abajo)**(1/3)
+print r[1]
